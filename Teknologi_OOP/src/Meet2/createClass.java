156feb3ac559a0fd1f496c72116a31a845e301da
farhannurhidayah <nurhidayahfarhan265@gmail.com>
2023-03-15 22:46:24 +0700

    membuat file createClass.java

---
+++ Teknologi_OOP/src/Meet2/createClass.java
@@ -0,0 +1,11 @@
+// Mobil merupakan class atau objec
+// Berat dan warna dalah attributes
+// Menyetir dan mengerem adalah method
+
+public class createClass {
+    int x = 5;
+    public static void main (String [] args) {
+        createClass myObj = new createClass();
+        System.out.println(myObj.x);
+    }
+}
