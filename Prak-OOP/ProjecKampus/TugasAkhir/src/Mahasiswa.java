46a308a2dd474228ee98fe298da158cc04a869bd
farhannurhidayah <nurhidayahfarhan265@gmail.com>
2023-05-09 23:45:13 +0700

    initial

---
+++ Prak-OOP/ProjecKampus/TugasAkhir/src/Mahasiswa.java
@@ -0,0 +1,11 @@
+public class Mahasiswa { 
+    public String nim;
+    public String nama;
+    public String kelas;
+    
+    public Mahasiswa(String nim, String nama, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+    }
+}
